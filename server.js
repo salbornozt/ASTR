09c8bf75271ba5ca63ae81c336a410cddb1f61c0
Santiago Albornoz Tibasoza <salbornozt@unbosque.edu.co>
2022-05-30 11:09:18 -0500

    fix servr

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/@fuse/starter'));
+app.use(express.static(__dirname + '/dist/fuse'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/@fuse/starter/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/fuse/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
